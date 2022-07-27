8b32d8778b61c3754cfe25cbfd098a7b6b5c4b1e
Sourabh Chauhan <sourabh.kumar35@gmail.com>
2022-07-27 14:21:41 +0530

    commit

---
+++ redux-shopping-cart-main/src/components/Navbar.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import img1 from '../components/img1.jpg';
+
+const Navbar = () => {
+    const items = useSelector((state) => state.cart);
+    return (
+        <div
+            style={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+            }}
+        >
+            <span className="logo">
+            <img style={{width: '80px', height: '70px'}} src={img1} alt=""/> 
+            </span>
+           
+         
+            <div>
+                <Link className="navLink" to="/">
+                    Home
+                </Link>
+                <Link className="navLink" to="/cart">
+                    Cart
+                </Link>
+                <span className="cartCount">Cart items: {items.length}</span>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
